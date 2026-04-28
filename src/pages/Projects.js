1a8134afdd4da3d53e2a0bcad413f04c0085e663
Laxminarayan Rajbhar <rajbhar.laxminarayan01@gmail.com>
2026-04-28 23:14:17 +0530

    update: New changes

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,18 +6,33 @@ function Projects() {
     <div className="projects-container">
       <h2 className="projects-title">My Projects</h2>
       <div className="projects-grid">
+
         <div className="project-card">
           <h3 className="project-title">Portfolio Website</h3>
           <p className="project-description">
             A colorful React + Tailwind portfolio showcasing my skills.
           </p>
         </div>
+
         <div className="project-card">
-          <h3 className="project-title">React Navbar</h3>
+          <h3 className="project-title">
+            I created a Youtube-like app that I called "RollamRoll."
+          </h3>
           <p className="project-description">
-            Custom responsive navigation bar with smooth styling.
+            Check this "RollamRoll" App via this Link:{" "}
+            <a href="https://laxminarayan-rajbhar.github.io/youtube-frontend"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Click Here 🔗
+            </a>
+            {" "}— The RollamRoll app is the desktop version of Laxminarayan Rajbhar's
+            video-sharing website. It serves as a significant entertainment and
+            social media platform by enabling users to view, share, search, and
+            publish videos on desktops and tablets.
           </p>
         </div>
+
       </div>
     </div>
   );
