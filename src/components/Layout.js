c45c1bc6a91ef0efeb839ae9db2b6abab12cb04b
Laxminarayan Rajbhar <rajbhar.laxminarayan01@gmail.com>
2026-05-06 09:57:00 +0530

    update: New changes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,37 @@
-import React from "react";
-import Navbar from "./Navbar";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import "./Layout.css";
 
 function Layout() {
+  const [exited, setExited] = useState(false);
+
+  if (exited) {
+    return (
+      <div className="exit-screen">
+        <p>You have exited the app.</p>
+      </div>
+    );
+  }
+
   return (
-    <div>
+    <div className="layout-wrapper">
+
+      {/* Navbar at top */}
       <Navbar />
-      <Outlet /> {/* ✅ This renders the current page inside Layout */}
+
+      {/* Page content */}
+      <main className="layout-content">
+        <Outlet />
+      </main>
+
+      {/* Close button at bottom */}
+      <div className="close-bar">
+        <button className="close-btn" onClick={() => setExited(true)}>
+          ✕ Exit App
+        </button>
+      </div>
+
     </div>
   );
 }
